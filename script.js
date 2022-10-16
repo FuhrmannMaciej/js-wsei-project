aff7f560937aa7b842dd5c119f06a7df2f57872b
fuhrmann.maciej <fuhrmann.maciej@gmail.com>
2022-10-16 09:53:26 +0200

    small refactor

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ const srednia = document.querySelector("srednia")
 const min = document.querySelector("min")
 const max = document.querySelector("max")
 
-// var a = document.querySelector("#numberOne").addEventListener('input', (number) => {
+// let a = document.querySelector("#numberOne").addEventListener('input', (number) => {
 // });
 
 function przelicz() {
-    var a = parseInt(document.querySelector("#numberOne").value);
-    var b = parseInt(document.querySelector("#numberTwo").value);
-    var c = parseInt(document.querySelector("#numberThree").value);
-    var d = parseInt(document.querySelector("#numberFour").value);
+    let a = parseInt(document.querySelector("#numberOne").value);
+    let b = parseInt(document.querySelector("#numberTwo").value);
+    let c = parseInt(document.querySelector("#numberThree").value);
+    let d = parseInt(document.querySelector("#numberFour").value);
     console.log("Suma: ")
     console.log(a + b + c + d)
     console.log("Srednia: ")
