1d13e7fa038c2a25571cb39ad5808a239e9e41e4
fuhrmann.maciej <fuhrmann.maciej@gmail.com>
2022-11-11 11:44:22 +0100

    Added css to lab 1, refactored and fixed przelicz

--- lab1/script.js
+++ lab1/script.js
@@ -1,22 +1,23 @@
-const suma = document.querySelector("suma")
-const srednia = document.querySelector("srednia")
-const min = document.querySelector("min")
-const max = document.querySelector("max")
+let suma = document.querySelector("#suma")
+let srednia = document.querySelector("#srednia")
+let min = document.querySelector("#min")
+let max = document.querySelector("#max")
 
-// let a = document.querySelector("#numberOne").addEventListener('input', (number) => {
-// });
+
 
 function przelicz() {
     let a = parseInt(document.querySelector("#numberOne").value);
     let b = parseInt(document.querySelector("#numberTwo").value);
     let c = parseInt(document.querySelector("#numberThree").value);
     let d = parseInt(document.querySelector("#numberFour").value);
-    console.log("Suma: ")
-    console.log(a + b + c + d)
-    console.log("Srednia: ")
-    console.log((a + b + c + d)/4)
-    console.log("Min: ")
-    console.log(Math.min(a,b,c,d))
-    console.log("Max: ")
-    console.log(Math.max(a,b,c,d))
+
+    a = a || 0
+    b = b || 0
+    c = c || 0
+    d = d || 0
+
+    suma.querySelector("span").innerText = a + b + c + d
+    srednia.querySelector("span").innerText = (a + b + c + d)/4
+    min.querySelector("span").innerText = Math.min(a,b,c,d)
+    max.querySelector("span").innerText = Math.max(a,b,c,d)
 }
