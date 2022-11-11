faac60c4b35ad9c255c5c696b32949f522c00616
fuhrmann.maciej <fuhrmann.maciej@gmail.com>
2022-11-11 15:40:23 +0100

    Added function for calculations, code refactored

--- lab1/script.js
+++ lab1/script.js
@@ -3,11 +3,13 @@ let srednia = document.querySelector("#srednia")
 let min = document.querySelector("#min")
 let max = document.querySelector("#max")
 
-let numberOne = document.querySelector("#numberOne")
+let inputContainer = document.querySelector("#inputs")
+
+let numberOne = document.querySelector("#number1")
 numberOne.addEventListener('input', przelicz)
-let numberTwo = document.querySelector("#numberTwo")
+let numberTwo = document.querySelector("#number2")
 numberTwo.addEventListener('input', przelicz)
-let numberThree = document.querySelector("#numberThree")
+let numberThree = document.querySelector("#number3")
 numberThree.addEventListener('input', przelicz)
 let numberFour = document.querySelector("#numberFour")
 numberFour.addEventListener('input', przelicz)
@@ -24,7 +26,26 @@ function przelicz() {
     d = d || 0
 
     suma.querySelector("span").innerText = a + b + c + d
-    srednia.querySelector("span").innerText = (a + b + c + d)/4
-    min.querySelector("span").innerText = Math.min(a,b,c,d)
-    max.querySelector("span").innerText = Math.max(a,b,c,d)
+    srednia.querySelector("span").innerText = (a + b + c + d) / 4
+    min.querySelector("span").innerText = Math.min(a, b, c, d)
+    max.querySelector("span").innerText = Math.max(a, b, c, d)
+}
+
+function calcSuma(...args) {
+    return args.reduce(function (a, b) {
+        return a + b
+    })
+}
+
+function calcSrednia(...args) {
+    let length = args.length
+    return calcSuma(args) / length
+}
+
+function dodajPole() {
+    let input = document.createElement("input")
+    input.type = "number"
+    input.placeholder = "0"
+
+    inputContainer.appendChild(input)
 }
