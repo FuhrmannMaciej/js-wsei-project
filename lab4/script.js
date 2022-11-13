1b07444339df4ef5f4181a928094f1f373d6a499
Maciej Fuhrmann <maciej.fuhrmann@microsoft.wsei.edu.pl>
2022-11-13 12:47:05 +0100

    lab 4 initial commit

---
+++ lab4/script.js
@@ -0,0 +1,37 @@
+let notesContainer = document.querySelector("#notes")
+
+class Note {
+
+    constructor(title, content, color, isPinned, createDate) {
+        this.title = title
+        this.content = content
+        this.color = color
+        this.isPinned = isPinned
+        this.createDate = createDate
+    }
+}
+
+function randomColor() {
+    var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+function addNote() {
+    let noteObject = new Note ("", "", randomColor(), false, Date.now())
+    let note = document.createElement("div")
+    note.className = "note"
+    note.style.setProperty("background-color", noteObject.color)
+
+    let title = document.createElement("input")
+    title.className = "title"
+    let content = document.createElement("input")
+    content.className = "content"
+
+    notesContainer.appendChild(note)
+    note.appendChild(title)
+    note.appendChild(content)
+}
