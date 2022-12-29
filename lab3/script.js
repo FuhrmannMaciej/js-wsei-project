ee512e49590e04238dcf0cef2869920c22444ed8
fuhrmann.maciej <fuhrmann.maciej@gmail.com>
2022-12-29 18:25:28 +0100

    changed audio channels to radio buttions, added new functions to handle recording

--- lab3/script.js
+++ lab3/script.js
@@ -1,4 +1,24 @@
 document.addEventListener('keypress', onKeyPress)
+const recordOne = document.querySelector('#a1')
+const recordTwo = document.querySelector('#a2')
+const recordThree = document.querySelector('#a3')
+const recordFour = document.querySelector('#a4')
+
+const playButton = document.querySelector('#playRecording')
+const playAllButton = document.querySelector('#playAll')
+const recordButton = document.querySelector('#toggleRecord')
+
+const audioRecordings = document.querySelectorAll('.recording')
+
+let recordingStartTime
+let songNotes
+let selectedAudio
+
+let audioStorageOne
+let audioStorageTwo
+let audioStorageThree
+let audioStorageFour
+
 
 const KeyToSound = {
     'a': 's1',
@@ -18,12 +38,67 @@ function playSound(sound) {
     if (!sound) {
         return
     }
+    if (isRecording()) {
+        recordNote(sound)
+    }
     const audioTag = document.querySelector(`#${sound}`)
     audioTag.currentTime = 0
     audioTag.play()
 }
 
-function record() {
-
+function toggleRecord() {
+    recordButton.classList.toggle('active')
+    if (isRecording()) {
+        startRecording()
+    } else {
+        stopRecording()
+    }
 }
-// Date.now()
+
+function isRecording() {
+    return recordButton != null && recordButton.classList.contains('active')
+  }
+
+function startRecording() {
+    recordingStartTime = Date.now()
+    isAudioRecordingChecked()
+    songNotes = []
+}
+
+function stopRecording() {
+    let storage = chooseStorage(selectedAudio) 
+    storage = Array.from(songNotes)
+    console.log(songNotes)
+    console.log(audioStorageOne)
+}
+
+function chooseStorage(storage) {
+    switch (storage) {
+        case "a1":
+            return audioStorageOne
+        case "a2":
+            return audioStorageTwo
+        case "a3":
+            return audioStorageThree
+        case "a4":
+            return audioStorageFour
+
+        default:
+            break;
+    }
+}
+
+function recordNote(note) {
+    songNotes.push({
+        key: note,
+        startTime: Date.now() - recordingStartTime
+    })
+}
+
+function isAudioRecordingChecked() {
+    for (const audio of audioRecordings) {
+        if (audio.checked) {
+            selectedAudio = audio.id
+        }
+    }
+}
