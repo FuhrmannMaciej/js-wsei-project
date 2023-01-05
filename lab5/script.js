489771c0b65673a0deb007d1bee4ca2f6f777a61
fuhrmann.maciej <fuhrmann.maciej@gmail.com>
2023-01-05 17:45:43 +0100

    lab 5 small fix to measureExecTime function

--- lab5/script.js
+++ lab5/script.js
@@ -19,18 +19,19 @@ async function addAll(...args) {
   return result;
 }
 
-function measureExecutionTime(callback) {
+async function measureExecutionTime(callback) {
   const startTime = performance.now();
-  callback();
+  await callback();
   const endTime = performance.now();
   const elapsedTime = endTime - startTime;
   console.log(`Execution time: ${elapsedTime}ms`);
 }
 
-addAll(2, 3, 4, 5).then(function (result) {
-  console.log(result)
-})
-
-measureExecutionTime(() => {
-  addAll(51,613,612,32);
+measureExecutionTime( async () => {
+  await addAll(41, 7, 84, 41, 28, 100, 2, 22, 16, 37, 45, 82, 72, 38, 18, 43, 48,
+    4, 14, 69, 99, 13, 30, 36, 77, 66, 69, 61, 89, 74, 77, 29, 37, 5, 61, 99, 32, 87, 62,
+    57, 26, 75, 12, 62, 52, 5, 24, 67, 39, 28, 57, 3, 26, 12, 58, 35, 92, 27, 65, 99, 58, 79, 20, 79,
+    70, 31, 30, 46, 66, 64, 35, 15, 20, 3, 57, 66, 46, 9, 91, 58, 93, 54, 88, 69, 30, 52, 53, 90, 43, 12, 67, 23, 99, 70, 44, 42, 70, 1, 57, 71).then(function (result) {
+      console.log(result)
+    });
 });
