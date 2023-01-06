eb3d9b461b7f05699dcbe3f29bceb30b48ac4ead
fuhrmann.maciej <fuhrmann.maciej@gmail.com>
2023-01-06 14:04:47 +0100

    lab6 added slider to change the number of balls, added calculation of distance and draw line if lower than threshold

--- lab6/script.js
+++ lab6/script.js
@@ -3,8 +3,10 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 let balls = [];
+const threshold = 200;
 
 function createBalls(numBalls) {
+  balls = [];
   for (let i = 0; i < numBalls; i++) {
     const x = Math.random() * (canvas.width - 100) + 50;
     const y = Math.random() * (canvas.height - 100) + 50;
@@ -21,22 +23,37 @@ function drawBalls() {
   const ctx = canvas.getContext('2d');
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  balls.forEach(ball => {
+  balls.forEach(ball1 => {
     ctx.beginPath();
-    ctx.arc(ball.x, ball.y, 20, 0, Math.PI * 2);
+    ctx.arc(ball1.x, ball1.y, 20, 0, Math.PI * 2);
     ctx.fillStyle = '#0095dd';
     ctx.fill();
     ctx.closePath();
 
-    ball.x += ball.dx;
-    ball.y += ball.dy;
+    ball1.x += ball1.dx;
+    ball1.y += ball1.dy;
 
-    if (ball.x + 20 > canvas.width || ball.x - 20 < 0) {
-      ball.dx = -ball.dx;
+    if (ball1.x + 20 > canvas.width || ball1.x - 20 < 0) {
+      ball1.dx = -ball1.dx;
     }
-    if (ball.y + 20 > canvas.height || ball.y - 20 < 0) {
-      ball.dy = -ball.dy;
+    if (ball1.y + 20 > canvas.height || ball1.y - 20 < 0) {
+      ball1.dy = -ball1.dy;
     }
+
+    balls.forEach(ball2 => {
+      if (ball1 !== ball2) {
+        const distance = Math.sqrt(
+          Math.pow(ball1.x - ball2.x, 2) + Math.pow(ball1.y - ball2.y, 2)
+        );
+        if (distance < threshold) {
+          ctx.beginPath();
+          ctx.moveTo(ball1.x, ball1.y);
+          ctx.lineTo(ball2.x, ball2.y);
+          ctx.strokeStyle = '#0095dd';
+          ctx.stroke();
+        }
+      }
+    });
   });
 }
 
@@ -44,6 +61,11 @@ function animate() {
   requestAnimationFrame(animate);
   drawBalls();
 }
+
+const slider = document.querySelector('#slider');
+slider.addEventListener('input', () => {
+  createBalls(slider.value);
+});
 
 createBalls(20);
 animate();
