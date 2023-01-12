53bc1ba6c93549055adf75d64cfee831f9707539
fuhrmann.maciej <fuhrmann.maciej@gmail.com>
2023-01-12 12:18:20 +0100

    lab7 code refactored, added fetch for weather data

--- lab7/script.js
+++ lab7/script.js
@@ -4,21 +4,38 @@ const searchResult = document.querySelector("#search-result");
 
 const apiKey = "7ed801ab1538fc4324b7440c5c303204";
 
+let cityLat;
+let cityLon;
+
 searchButton.addEventListener("click", function () {
-  fetch("http://api.openweathermap.org/geo/1.0/direct?q=" + searchInput.value + "&appid=" + apiKey)
+  findCity();
+});
+
+function findCity() {
+  const cityName = searchInput.value;
+  fetch("http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&appid=" + apiKey)
     .then(response => response.json())
     .then(data => {
+      console.log(data);
       searchResult.innerHTML = "";
-      data.forEach(item => {
-        if (item.name.toLowerCase() === searchInput.value.toLowerCase()) {
-          const itemElement = document.createElement("li");
-          itemElement.innerHTML = item.name;
-          searchResult.appendChild(itemElement);
-        }
-      });
+      cityLat = data[0].lat;
+      cityLon = data[0].lon;
+      const itemElement = document.createElement("li");
+      itemElement.innerHTML = data[0].name;
+      searchResult.appendChild(itemElement);
+      console.log(cityLat);
+      getCurrentWeatherData();
     })
     .catch(error => {
       console.error("Error:", error);
       searchResult.innerHTML = "An error occurred. Please try again later.";
     })
-  });
+}
+function getCurrentWeatherData() {
+  fetch("https://api.openweathermap.org/data/2.5/weather?lat=" + cityLat + "&lon=" + cityLon + "&units=metric" + "&appid=" + apiKey)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+    });
+}
+
